85d28b30e1ae4530c362598f5026d4da0629a835
Gael Huber <gael.scott.huber@gmail.com>
2026-01-12 08:20:54 -0800

    search hidden files in telescope

--- dotfiles/nvim/lua/gdot/plugins/telescope.lua
+++ dotfiles/nvim/lua/gdot/plugins/telescope.lua
@@ -9,7 +9,18 @@ return {
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
+    local telescope_config = require("telescope.config")
 
+    -- Clone the default Telescope configuration
+    local vimgrep_arguments = { unpack(telescope_config.values.vimgrep_arguments) }
+
+    -- Search hidden/dot files
+    table.insert(vimgrep_arguments, "--hidden")
+
+    -- Don't search `.git` directory
+    table.insert(vimgrep_arguments, "--glob")
+    table.insert(vimgrep_arguments, "!**/.git/*")
+    
     telescope.setup({
       defaults = {
         path_display = { "smart" },
@@ -19,6 +30,14 @@ return {
             ["<C-j>"] = actions.move_selection_next, -- move to next result
             ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
           },
+        },
+        -- `hidden = true` is not supported in text grep commands
+        vimgrep_arguments = vimgreg_arguments,
+      },
+      pickers = {
+        find_files = {
+          -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
         },
       },
     })
