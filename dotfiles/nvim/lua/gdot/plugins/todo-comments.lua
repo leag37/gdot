9ccf589b9e876b147be1df0f1a5834a8fa39a322
Gael Huber <gael.scott.huber@gmail.com>
2026-01-17 14:34:29 -0800

    add proper dependency for todo comments

--- dotfiles/nvim/lua/gdot/plugins/todo-comments.lua
+++ dotfiles/nvim/lua/gdot/plugins/todo-comments.lua
@@ -1,6 +1,7 @@
 return {
   "folke/todo-comments.nvim",
   event = { "BufReadPre", "BufNewFile" },
+  dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     local todo_comments = require("todo-comments")
     
