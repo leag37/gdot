727562d030f58f278d5be1397612e672a34e1644
Gael Huber <gael.scott.huber@gmail.com>
2026-01-17 07:05:47 -0800

    comment support in nvim

---
+++ dotfiles/nvim/lua/gdot/plugins/comment.lua
@@ -0,0 +1,13 @@
+return {
+  "numToStr/Comment.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+  },
+  config = function()
+    local comment = require("Comment")
+
+    -- enable comment
+    comment.setup()
+  end,
+}
