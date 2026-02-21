920f7337019cba4ba80f7b3cb4710aa7ebfce62c
Gael Huber <gael.scott.huber@gmail.com>
2026-02-20 19:45:40 -0800

    fix bad mapping in treesitter-textobjects

--- dotfiles/nvim/lua/gdot/plugins/treesitter-textobjects.lua
+++ dotfiles/nvim/lua/gdot/plugins/treesitter-textobjects.lua
@@ -136,8 +136,8 @@ return {
     local ts_repeat_move = require("nvim-treesitter-textobjects.repeatable_move")
     keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
     keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
-    keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_t_expr, { expr = true })
-    keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_T_expr, { expr = true })
+    keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+    keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
     keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
     keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
   end,
